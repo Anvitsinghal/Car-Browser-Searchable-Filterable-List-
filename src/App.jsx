4219ca2fef72a0c3793f5446c2e6fed18f85bc7d
anvit <singhalanvit534@gmail.com>
2026-07-01 15:31:28 +0530

    Add search bar with 300ms debounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import carsdata from "../src/data/Cars.json"
 console.log(carsdata)
 function App(){
   const [data,setData]=useState(carsdata)
+  const [search,setsearch]=useState("")
+  useEffect(()=>{
+   if (search.trim() === "") {
+    setData(carsdata);
+    return;
+}
+    const timer=setTimeout(() => {
+        const query=search.trim().toLowerCase()
+        const filteredData = carsdata.filter((car) =>car.name.toLowerCase().includes(query));
+        setData(filteredData)
+    }, 300);
+    return () => clearTimeout(timer);
+  },[search])
   return (
- <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+    <>
+<div className="p-4">
+  <input
+    type="text"
+    placeholder="Search car"
+    value={search}
+    onChange={(e) => setsearch(e.target.value)}
+    className="w-full max-w-md mx-auto block px-4 py-2 border border-gray-300 rounded-lg "
+  />
+</div>
+     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
   {data?.map((value) => (
     <div
       key={value?.id}
@@ -40,6 +63,8 @@ function App(){
     </div>
   ))}
 </div>
+</>
+
   );
 }
 export default App
