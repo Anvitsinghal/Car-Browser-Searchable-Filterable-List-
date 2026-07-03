55cd17a06271307fffd4cd858dfccd9647bd3f55
anvit <singhalanvit534@gmail.com>
2026-07-03 12:01:29 +0530

    added url mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from "react";
 import carsdata from "../src/data/Cars.json"
+import { useSearchParams } from "react-router-dom";
 console.log(carsdata)
 function App(){
+  const [searchParams, setSearchParams] = useSearchParams();
   const [data,setData]=useState(carsdata)
-  const [search,setsearch]=useState("")
-  const [transmission,setTranismission]=useState("");
-  const [Type,setType]=useState("");
-  const [Available,setAvailable]=useState(false)
-  const [sortprice,selectsortprice]=useState("")
+  const [search,setsearch]=useState(searchParams.get("search") || "")
+  const [transmission,setTranismission]=useState(searchParams.get("transmission") || "");
+  const [Type,setType]=useState(searchParams.get("type") || "");
+  const [Available,setAvailable]=useState(searchParams.get("available") === "true")
+  const [sortprice,selectsortprice]=useState(searchParams.get("sortPrice") || "")
   const [len,setlen]=useState(carsdata.length)
   useEffect(()=>{
-   if (search.trim() === ""&& transmission==""&&Type==""&& Available=="") {
+  if (search.trim() === "" && transmission === "" && Type === "" && sortprice==""&&  !Available) {
     setData(carsdata);
     return;
 }
     const timer=setTimeout(() => {
         const query=search.trim().toLowerCase()
         const filteredData = carsdata.filter((car) =>(car.name.toLowerCase().includes(query))&&
-      car.transmission.toLowerCase().includes(transmission.toLocaleLowerCase())&&car.type.toLowerCase().includes(Type.toLocaleLowerCase())&&car.available==Available
+      car.transmission.toLowerCase().includes(transmission.toLocaleLowerCase())&&car.type.toLowerCase().includes(Type.toLocaleLowerCase())&&(!Available || car.available)
       );
       setlen(filteredData.length)
       if (sortprice === "0") {
@@ -36,6 +38,7 @@ else if (sortprice === "1") {
     setType("");
     setAvailable(false);
     selectsortprice("");
+    setSearchParams({});
 };
 
   return (
@@ -45,35 +48,79 @@ else if (sortprice === "1") {
     type="text"
     placeholder="Search car"
     value={search}
-    onChange={(e) => setsearch(e.target.value)}
+    onChange={(e) => {setsearch(e.target.value)
+      const newParams = new URLSearchParams(searchParams);
+      if (e.target.value) {
+      newParams.set("search", e.target.value);
+    } else {
+      newParams.delete("search");
+    }
+    setSearchParams(newParams);
+    }
+    }
     className="w-full max-w-md mx-auto block px-4 py-2 border border-gray-300 rounded-lg "
   />
-  <select onChange={(e)=>setType(e.target.value)}>--Type--
-    <option value="">All</option>
+  <div className="flex space-between gap-5">
+  <select className="w-full md:w-auto bg-white border border-gray-300 text-gray-700 py-2 px-4 " value={Type} onChange={(e)=>{setType(e.target.value)
+     const newParams = new URLSearchParams(searchParams);
+      if (e.target.value) {
+      newParams.set("type", e.target.value);
+    } else {
+      newParams.delete("type");
+    }
+    setSearchParams(newParams);
+  }}>--Type--
+    <option value="">--Car Type--</option>
     <option value="Economy">Economy</option>
     <option value="Sedan">Sedan</option>
     <option value="SUV">SUV</option>
     <option value="Luxury">Luxury</option>
   </select>
  
-   <select onChange={(e)=>setTranismission(e.target.value)}>--Type--
-      <option value="">All</option>
+   <select className="w-full md:w-auto bg-white border border-gray-300 text-gray-700 py-2 px-4 " value={transmission} onChange={(e)=>{setTranismission(e.target.value)
+    const newaparams=new URLSearchParams(searchParams)
+       if (e.target.value) {
+      newaparams.set("transmission", e.target.value);
+    } else {
+      newaparams.delete("transmission");
+    }
+    setSearchParams(newaparams);
+   }}>
+      <option value="">--Engine Type--</option>
     <option value="Automatic">Automatic</option>
     <option value="Manual">Manual</option>
   
   </select>
-    <select onChange={(e)=>selectsortprice(e.target.value)}>--Price--
+    <select className="w-full md:w-auto bg-white border border-gray-300 text-gray-700 py-2 px-4 "  value={sortprice} onChange={(e)=>{selectsortprice(e.target.value)
+      const newaparams=new URLSearchParams(searchParams)
+       if (e.target.value) {
+      newaparams.set("sortPrice", e.target.value);
+    } else {
+      newaparams.delete("sortPrice");
+    }
+    setSearchParams(newaparams);
+
+    }}>--Price--
     <option value="0">Price Low to High</option>
     <option value="1">Price High to Low</option>
   
   </select>
-   <input
-  type="checkbox"
-  checked={Available}
-  onChange={(e) => setAvailable(e.target.checked)}
-/>
-   <div>{`Showing ${len} of ${carsdata.length} cars`}</div>
+<label className="flex items-center gap-2 cursor-pointer select-none">
+  <input
+    type="checkbox"
+    checked={Available}
+    onChange={(e) => {
+      setAvailable(e.target.checked);
+      updateURL("available", e.target.checked); 
+    }}
+    className="w-4 h-4 text-blue-600 rounded border-gray-300 "
+  />
+  <span className="text-gray-700 font-medium">Available Only</span>
+</label>
 </div>
+   <div>{`Showing ${data.length} of ${carsdata.length} cars`}</div>
+</div>
+
 
      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
     {data.length==0 &&  (
